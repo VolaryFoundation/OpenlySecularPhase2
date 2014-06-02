3822d22e195b0f878ea6d970253abc26e3c082ff
Nick Johannsen <Nicholas.Johannsen@gmail.com>
2014-06-02 12:43:10 -0700

    Added in activism templet widget

---
+++ app/widgets/activism/main.js
@@ -0,0 +1,43 @@
+
+var asWidget = require('widget')
+var rivets = require('rivets')
+
+// require all widgets
+
+rivets.adapters['.'] = {
+  subscribe: function(obj, keypath, callback) { },
+  unsubscribe: function(obj, keypath, callback) { },
+  read: function(obj, keypath) {
+    return obj[keypath]
+  },
+  publish: function(obj, keypath, value) {
+    obj[keypath] = value
+    return obj
+  }
+}
+
+rivets.adapters[':'] = {
+  subscribe: function(obj, keypath, callback) {
+    obj.on('change:' + keypath, callback)
+  },
+  unsubscribe: function(obj, keypath, callback) {
+    obj.off('change:' + keypath, callback)
+  },
+  read: function(obj, keypath) {
+    return obj.get(keypath)
+  },
+  publish: function(obj, keypath, value) {
+    obj.set(keypath, value)
+  }
+}
+
+asWidget('activism', function(hub) {
+  var widget = this
+
+  widget.template('/widgets/activism/index.html')
+  widget.on('installed', function() {
+    widget.start()
+  })
+})
+
+module.exports = {}
